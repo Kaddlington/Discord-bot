36832f2a2e5e3cc5efc9457fae8a6e40fff3c2d0
Kaddlington <kadd.grantblissett@gmail.com>
2025-07-27 00:39:36 +0100

    ZTM Course - Discord-Bot

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ]
+});
+
+client.on(Events.ClientReady, () => {
+    console.log('Logged in!');
+});
+
+client.login();
