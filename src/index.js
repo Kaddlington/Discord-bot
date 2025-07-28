f7fdfcb771663c443311023780284ebc330bd033
Kaddlington <kadd.grantblissett@gmail.com>
2025-07-29 00:00:38 +0100

    Adds forecast command
    Adds a new command to fetch and display weather forecasts for specific locations.
    
    Also, handles errors when fetching forecasts for a location to provide a better user experience.
    
    Adds forecast command
    
    Adds a new command to fetch and display weather forecasts for specific locations.
    
    Improves user experience by handling errors when fetching forecasts for a location.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { clientReadyHandler } = require('./events/clientReady');
-const pingCommand = require('./commands/ping');
 const { interactionCreateHandler } = require('./events/interactionCreate');
+
+const pingCommand = require('./commands/ping');
+const forecastCommand = require('./commands/forecast');
 
 const client = new Client({
     intents: [
@@ -13,6 +15,7 @@ const client = new Client({
 client.commands = new Collection();
 
 client.commands.set(pingCommand.data.name, pingCommand);
+client.commands.set(forecastCommand.data.name, forecastCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
 
