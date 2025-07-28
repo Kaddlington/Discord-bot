57e644f677aecb5f42da118322dd9533392c9451
Kaddlington <kadd.grantblissett@gmail.com>
2025-07-29 00:40:44 +0100

    Adds astro command
    Adds the astro command and includes sunrise, sunset, moonrise, and moonset times to the weather forecast.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { clientReadyHandler } = require('./events/clientReady');
 const { interactionCreateHandler } = require('./events/interactionCreate');
 
+
 const pingCommand = require('./commands/ping');
 const forecastCommand = require('./commands/forecast');
+const astroCommand = require('./commands/astro');
 
 const client = new Client({
     intents: [
@@ -16,6 +18,7 @@ client.commands = new Collection();
 
 client.commands.set(pingCommand.data.name, pingCommand);
 client.commands.set(forecastCommand.data.name, forecastCommand);
+client.commands.set(astroCommand.data.name, astroCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
 
